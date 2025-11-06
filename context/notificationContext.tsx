1221c37fbe7d47980efc0939949c69ebfcf46550
PramisGrg <gurungpramis361@gmail.com>
2025-11-06 17:28:36 +0545

    feat: added notification context

---
+++ context/notificationContext.tsx
@@ -0,0 +1,71 @@
+import { registerForPushNotificationsAsync } from "@/utils/registerForPushNotificationAsync";
+import * as Notifications from "expo-notifications";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+interface NotificationContextType {
+  expoPushToken: string | null;
+  notification: Notifications.Notification | null;
+  error: Error | null;
+}
+
+const NotificationContext = createContext<NotificationContextType | undefined>(
+  undefined
+);
+
+export const useNotification = () => {
+  const context = useContext(NotificationContext);
+  if (context === undefined) {
+    throw new Error("useNotification must be within notification context");
+  }
+  return context;
+};
+
+interface NotificationProviderProps {
+  children: ReactNode;
+}
+
+export const NotificationProvider = ({
+  children,
+}: NotificationProviderProps) => {
+  const [expoPushToken, setExpoPushToken] = useState<string | null>(null);
+  const [notification, setNotification] =
+    useState<Notifications.Notification | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(
+      (token) => token && setExpoPushToken(token),
+      (error) => setError(error)
+    );
+
+    const notificationListener = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        setNotification(notification);
+      }
+    );
+
+    const responseListener =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      notificationListener.remove();
+      responseListener.remove();
+    };
+  }, []);
+
+  return (
+    <NotificationContext.Provider
+      value={{ expoPushToken, notification, error }}
+    >
+      {children}
+    </NotificationContext.Provider>
+  );
+};
